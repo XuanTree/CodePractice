d8fd6e927396ddf74c9f8890124ea85f9ca6f35e
XuanTree <3304748409@qq.com>
2025-10-02 17:45:07 +0800

    C++

---
+++ C&C++/Week_4/ch4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double num;
+    
+    while(scanf("%lf",&num) != EOF)
+    {
+        if (num >= 0 && num < 1000)
+        {
+            if (floor(sqrt(num)) != sqrt(num))
+            {
+                int result = sqrt(num);
+                printf("%d",result);
+            }
+            else
+            {
+                printf("%.2lf",sqrt(num));
+            }
+            break;
+        }
+        else
+        {
+            printf("请重新输入一个正数\n");
+            continue;
+        }
+    }
+    return 0;
+}
---
+++ C&C++/Week_4/ch4_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i <= 5)
+    {
+        char num[i];
+    }
+    else
+    {
+        printf("");
+    }
+    return 0;
+}
